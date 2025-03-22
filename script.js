325cb46cfadcc90627340f007a004b1604aee65b
someloki <tenacity686@gmail.com>
2025-03-22 00:30:04 -0700

    Feat(script.js): Add createElement function

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 // start with 16 boxes arranged in 4x4 grid
 
 // have button that when pressed accepts a number from the user and translates to new total boxes
+const footer = document.querySelector(".footer")
+// const btn = document.createElement("button");
+// btn.textContent = "Click to Change";
+// btn.classList.toggle("change");
+// footer.appendChild(btn);
 
+const createElement = function(elem, classToggle, textContent, appendTo) {
+  const element =  document.createElement(elem);
+  element.classList.toggle(classToggle);
+  element.textContent = textContent;
+  appendTo.appendChild(element);
+}
+
+createElement("button", "changeGrid", "Click to Change", footer);
+createElement("button", "resetGrid", "Click to Reset", footer);
 // find equal or highest perfect square to know what sort of grid to form
 
 // divide current content box width / height. Whichever is lower set as both width and height of boxes - they must be square
@@ -9,13 +23,13 @@
 // generate boxes through iterative loops. Will need a counter for box number, increment row. class will have to be generated and tied to boxes so need the numbers
 
 
-
+// to use naming boxes 1-100
 const rowNames = [];
 const columnNames = [];
 const lowerNumPrefix = ["first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth", "tenth"];
 const UpperNumPrefix = ["First", "Second", "Third", "Fourth", "Fifth", "Sixth", "Seventh", "Eighth", "Ninth", "Tenth"];
 
-for (i = 0; i < 10; i++) {
+for (let i = 0; i < 10; i++) {
   rowNames[i] = ( lowerNumPrefix[i] + "Row");
   columnNames[i] = (UpperNumPrefix[i] + "Column");
 }
