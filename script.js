0b094d3db165f07357375f154ff4b85022b2c15a
someloki <tenacity686@gmail.com>
2025-03-23 15:18:17 -0700

    Fix(script.js): Rework funcs align w/ project
    Was built with logic of total boxes instead of box/side.
    Remove unneeded functions as a result.

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
-// start with 16 boxes arranged in 4x4 grid
 
-const footer = document.querySelector(".footer")
+const header = document.querySelector(".header")
 const sketchArea = document.querySelector(".sketchzone");
 
 const rowNames = [];
 const columnNames = [];
-const lowerNumPrefix = ["first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth", "tenth"];
-const UpperNumPrefix = ["First", "Second", "Third", "Fourth", "Fifth", "Sixth", "Seventh", "Eighth", "Ninth", "Tenth"];
 
-for (let i = 0; i < 10; ++i) {
-  rowNames[i] = ( lowerNumPrefix[i] + "Row");
-  columnNames[i] = (UpperNumPrefix[i] + "Column");
+
+for (let i = 0; i < 100; ++i) {
+  rowNames[i] = ("Row" + `${i}`);
+  columnNames[i] = ("Column" + `${i}`);
 }
 
 
@@ -19,32 +17,25 @@ const createElement = function(elem, addClass, textContent, appendTo) {
   element.classList.add(addClass);
   element.textContent = textContent;
   appendTo.appendChild(element);
-
 }
 
-// need to add clearing old boxes
-const startBoxBuilding = function(boxCount) {
-  const gridLayout = perfectSquare(boxCount);
+const buildGrid = function(sideCount) {
+  let totalBoxes = (sideCount * sideCount)
   let columns = 0;
   let rows = 0;
   let i = 0;
-  const rowDiff = Math.ceil(boxCount / gridLayout);
-  console.log(rowDiff);
   clearSketchZone()
-    for (rows; ( (rows <= gridLayout) && (i < boxCount)) ;) {
-      columns = 0;
-      for (columns; ((columns < gridLayout) && (i <boxCount)) ; columns++) {
-        const boxClass = (rowNames[rows] + columnNames[columns]);
-        createElement("div", boxClass,"", sketchArea)
-        setBoxSize(boxClass, gridLayout, rowDiff);
-        i++
-      }
-      rows++;
+  for (rows; ( (rows <= sideCount) && (i < totalBoxes)) ;) {
+    columns = 0;
+    for (columns; ((columns < sideCount) && (i < totalBoxes)) ; columns++) {
+      const boxClass = (rowNames[rows] + columnNames[columns]);
+      createElement("div", boxClass,"", sketchArea)
+      setBoxSize(boxClass, sideCount);
+      i++
     }
-}
-
-function test(i, row, col){
-  console.log(`loop: ${i} row: ${row} column: ${col}`)
+    rows++;
+  }
+  test();
 }
 
 const whichButton = function(){
@@ -52,30 +43,27 @@ const whichButton = function(){
   if (this.classList == "changeGrid"){
     let i = false;
     while (i === false) {
-      boxCount = Number(prompt( "How many boxes do you want? 1-100" ));
+      boxCount = Number(prompt( "How many boxes per side? 1-100" ));
       i = validateNum(boxCount);
     }
-    startBoxBuilding(boxCount);
+    buildGrid(boxCount);
   } else if (this.classList == "resetGrid"){
-    startBoxBuilding(16);
+    buildGrid(16);
   }
 }
 
 // helper functions in global scope
 
-function perfectSquare(num) {
-  let perfectSq = 0;
-  let i = 0;
-  while ( perfectSq < num ) {
-    i++
-    perfectSq = ( i * i );
-  }
-  return i;
+function setBoxSize(box, gridLayout) {
+  const boxElem = document.querySelector(`.${box}`);
+  const size = (100 / gridLayout);
+  boxElem.style.height = (`${size}%`);
+  boxElem.style.width = (`${size}%`);
 }
 
- function validateNum(num) {
+function validateNum(num) {
   return ((Number.isInteger(num) === true) && ((num > 0) && num <=100));
- } 
+} 
 
 function clearSketchZone() {
   while (sketchArea.lastElementChild) {
@@ -83,22 +71,27 @@ function clearSketchZone() {
   }
 }
 
-createElement("button", "changeGrid", "Click to Change", footer);
-createElement("button", "resetGrid", "Click to Reset", footer);
+createElement("button", "changeGrid", "Click to Change", header);
+createElement("button", "resetGrid", "Click to Reset", header);
+buildGrid(16);
 const buttons = document.querySelectorAll("button");
 
 buttons.forEach((button) => {
   button.addEventListener("click", whichButton);
 });
 
+function test () {
+const boxes = sketchArea.querySelectorAll("div");
 
-function setBoxSize(box, gridLayout, rowDiff) {
-const boxElem = document.querySelector(`.${box}`);
-const colSize = (100 / gridLayout);
-const rowSize  = (100 / rowDiff);
-boxElem.style.height = (`${rowSize}%`);
-boxElem.style.width = (`${colSize}%`);
+boxes.forEach((box) => {
+  box.addEventListener("mouseover", () => {
+    box.style.backgroundColor = "rgb( 0, 0, 0)";
+  });
+});
 }
+
+
+
 
 // still need to add the event listeners for actually monitoring mouse movement / boxes and applying coloring.
 
